7e8351750b9c8249648bc7e3d4d3e68ceae21388
Zalak Bhojani <zalakbhojani@yahoo.com>
2020-05-30 00:27:46 +0530

    Upload page completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/video/upload" component={Auth(UploadVideoPage, false)} />
+          <Route exact path="/video/upload" component={Auth(UploadVideoPage, true)} />
         </Switch>
       </div>
       <Footer />
