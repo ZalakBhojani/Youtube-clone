71bfd786ed8e63b53d558986195e814b6ce81e1e
Zalak Bhojani <zalakbhojani@yahoo.com>
2020-05-29 22:22:19 +0530

    UploadPage in progress

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,8 @@ import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
-import Footer from "./views/Footer/Footer"
+import Footer from "./views/Footer/Footer";
+import UploadVideoPage from "./views/UploadVideoPage/UploadVideoPage";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/video/upload" component={Auth(UploadVideoPage, false)} />
         </Switch>
       </div>
       <Footer />
---
+++ client/src/components/views/UploadVideoPage/UploadVideoPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UploadVideoPage() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UploadVideoPage;
