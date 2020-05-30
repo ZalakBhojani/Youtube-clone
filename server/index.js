57ad512eb1b074e33571507364d77f0b37a5a44d
Zalak Bhojani <zalakbhojani@yahoo.com>
2020-05-30 11:54:43 +0530

    upload in server using multer

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
-
+app.use('/api/video', require('./routes/video'));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
