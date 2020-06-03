f8e5e4df5b09f1a761b4f8e7ae07e632ec1019d1
Zalak Bhojani <zalakbhojani@yahoo.com>
2020-06-03 20:37:57 +0530

    making functionalites like like,dislike,comment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(proxy("/api", { target: "http://localhost:5000/" }));
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+        })
+    );
 };
